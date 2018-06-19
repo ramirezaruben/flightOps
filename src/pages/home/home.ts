ff3435e10d73cfaa429d811ec8d2ff9281b8c439
Ramirez <rramirez@corp.tcc.inet>
2018-06-19 11:11:42 -0400

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,private iab: InAppBrowser) {
+
+  }
+  ngOnInit(){
+    const browser = this.iab.create('http://demo.flightops.systems','_self',{location:'no'}); 
+}
+
+}
